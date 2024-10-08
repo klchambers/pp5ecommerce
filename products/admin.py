eb756ed8464db02da80cbabc9031bad315222df8
Kieran Chambers <kieran.chambers@icloud.com>
2024-10-08 23:52:15 +0100

    Feat: Register Products models in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Wine, Region, Category
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Wine)
+class WineAdmin(SummernoteModelAdmin):
+    # Fields to display in list view
+    list_display = ('name', 'winemaker', 'region', 'rating')
+    # Enable search on these fields
+    search_fields = ('name', 'winemaker', 'region__name', 'category__name')
+    # Filters in the sidebar
+    list_filter = ('region', 'category', 'rating')
+    # Auto-populate slug field from name
+    prepopulated_fields = {'slug': ('name',)}
+    # Horizontal filter for the ManyToManyField 'category'
+    filter_horizontal = ('category',)
+
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Region)
