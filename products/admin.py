7e96f201364ce35f448f6571a72f312af871df69
Kieran Chambers <kieran.chambers@icloud.com>
2024-10-09 14:34:08 +0100

    Feat: Improve layout admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,4 @@
+from django.utils.html import format_html
 from django.contrib import admin
 from .models import Wine, Region, Category, GrapeVariety
 from django_summernote.admin import SummernoteModelAdmin
@@ -6,7 +7,12 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Wine)
 class WineAdmin(SummernoteModelAdmin):
     # Fields to display in list view
-    list_display = ('name', 'winemaker', 'region', 'rating')
+    list_display = ('name',
+                    'winemaker',
+                    'region',
+                    'rating',
+                    'price',
+                    'image_preview')
     # Enable search on these fields
     search_fields = ('name', 'winemaker', 'region__name', 'category__name')
     # Filters in the sidebar
@@ -19,6 +25,11 @@ class WineAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     # Horizontal filter for the ManyToManyField 'category'
     filter_horizontal = ('category',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 200px;"/>', obj.image.url) # noqa
+        return "No Image"
 
 
 # Register your models here.
