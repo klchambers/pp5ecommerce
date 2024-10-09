299bcbf59347ce645040990d09f8a1ec60b8f137
Kieran Chambers <kieran.chambers@icloud.com>
2024-10-09 15:02:04 +0100

    Feat: Create view and template for /products

--- pp5ecommerce/urls.py
+++ pp5ecommerce/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Wine
+
 
 # Create your views here.
+def all_products(request):
+    """View to return all products, including sorting and search"""
+
+    products = Wine.objects.all()
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'products/products.html', context)
