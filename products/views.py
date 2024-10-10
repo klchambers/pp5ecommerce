87a7596e8bbb8a89573c3d6f09a623fffd42b9c7
Kieran Chambers <kieran.chambers@icloud.com>
2024-10-10 15:36:25 +0100

    Feat: Add search functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Wine
 
 
@@ -7,9 +9,23 @@ def all_products(request):
     """View to return all products, including sorting and search"""
 
     products = Wine.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,
+                               'Error: You did not enter a search term')
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query) # noqa
+
+            products = products.filter(queries)
 
     context = {
-        'products': products
+        'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
