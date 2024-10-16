e930a4a01e50fbb91caff34a108576a59b2e9811
Kieran Chambers <kieran.chambers@icloud.com>
2024-10-16 11:44:41 +0100

    Feat: Add bag HTML and logic

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, reverse, HttpResponse # noqa
+from django.contrib import messages
+from products.models import Wine
 
 
 # Create your views here.
@@ -21,3 +23,55 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ adjust a quantity of the specified product in the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    product = get_object_or_404(Wine, pk=item_id)
+
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(
+            request, f"Updated {product.name} quantity to {bag[item_id]}")
+    else:
+        bag.pop(item_id)
+        messages.success(
+            request,
+            f"Removed {product.name} quantity from {bag[item_id]}")
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    product = get_object_or_404(Wine, pk=item_id)
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]["items_by_size"][size]
+            if not bag[item_id]["items_by_size"]:
+                bag.pop(item_id)
+            messages.success(
+                    request,
+                    f"""Removed {size.upper()} {product.name} from the bag""")
+        else:
+            bag.pop(item_id)
+            messages.success(
+                request,
+                f"Removed {product.name} from your bag")
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(f"Error removing item: {e}")
+        return HttpResponse(status=500)
