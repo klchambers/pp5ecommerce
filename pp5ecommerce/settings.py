25e4ad5950a03270073b19b1c5abba392c13ee3f
Kieran Chambers <kieran.chambers@icloud.com>
2024-10-16 23:09:17 +0100

    Fix: Configure Cloudinary

--- pp5ecommerce/settings.py
+++ pp5ecommerce/settings.py
@@ -163,6 +163,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Cloudinary setup
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
