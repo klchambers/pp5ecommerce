701e92bd0656223006bdfcafe022500b0ffea66e
Kieran Chambers <kieran.chambers@icloud.com>
2024-10-22 17:19:55 +0100

    Feat: Add buttons for superuser to delete or edit products from product_info page

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_info, name='product_info'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/',
+         views.delete_product,
+         name='delete_product'),
     path('add/', views.add_product, name='add_product'),
 ]
