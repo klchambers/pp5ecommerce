799b018cfa3ebec14cf8fcb93421868c2147adf6
Kieran Chambers <kieran.chambers@icloud.com>
2024-10-22 13:55:35 +0100

    Feat: Add profiles templates, urls and model

--- pp5ecommerce/urls.py
+++ pp5ecommerce/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
