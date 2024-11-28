1de605a1bbdb835f464d17127b86b60ec28e13b1
Kieran Chambers <kieran.chambers@icloud.com>
2024-11-28 14:12:15 +0000

    Refactor: Remove redundant code from bag views

--- bag/views.py
+++ bag/views.py
@@ -60,23 +60,11 @@ def remove_from_bag(request, item_id):
 
     product = get_object_or_404(Wine, pk=item_id)
     try:
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
         bag = request.session.get('bag', {})
 
-        if size:
-            del bag[item_id]["items_by_size"][size]
-            if not bag[item_id]["items_by_size"]:
-                bag.pop(item_id)
-            messages.success(
-                    request,
-                    f"""Removed {product.friendly_name} from the bag""")
-        else:
-            bag.pop(item_id)
-            messages.success(
-                request,
-                f"Removed {product.friendly_name} from your bag")
+        bag.pop(item_id)
+        messages.success(
+            request, f"Removed {product.friendly_name} from your bag")
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
