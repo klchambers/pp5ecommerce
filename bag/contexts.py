d11461ba8910dc8c36ac227f50e6cbdf09612d06
Kieran Chambers <kieran.chambers@icloud.com>
2024-12-03 12:19:19 +0000

    Fix: Prevent errors by adding exception rule to bag_contents where a product does not exist

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,28 +1,40 @@
 from decimal import Decimal
 from django.conf import settings
-from django.shortcuts import get_object_or_404
+from django.contrib import messages
 from products.models import Wine
 
 
 def bag_contents(request):
+    """
+    Retreives and calculates contents of the user's shopping
+    bag from the session
 
+    If an item is no longer available (i.e. it has been deleted from the store)
+    then pop it from the bag and display an informative message to the user
+    """
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Wine, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, quantity in list(bag.items()):
+        try:
+            product = Wine.objects.get(pk=item_id)
+            total += quantity * product.price
+            product_count += quantity
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'product': product,
+            })
+        except Wine.DoesNotExist:
+            bag.pop(item_id)
+            messages.error(request, f'product with ID {item_id} was removed \
+                from the bag as this product no longer exists')
 
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
     grand_total = delivery + total
+    request.session['bag'] = bag
 
     context = {
         'bag_items': bag_items,
