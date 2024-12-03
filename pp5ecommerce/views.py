4b6c43f838c23c8c1f813d06aac358aa9f67c22c
Kieran Chambers <kieran.chambers@icloud.com>
2024-12-03 15:57:35 +0000

    Fix: Update 500 error handler

--- pp5ecommerce/views.py
+++ pp5ecommerce/views.py
@@ -8,8 +8,9 @@ def custom_404(request, exception):
     return render(request, 'errors/404.html', status=404)
 
 
-def custom_500(request, exception):
+def custom_500(request):
     """
-    Error handler that renders a friendly 404 page.
+    Error handler that renders a friendly 500 page to direct user
+    back to shop or profile.
     """
     return render(request, 'errors/500.html', status=500)
