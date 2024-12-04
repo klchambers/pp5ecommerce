4e58b30763de7c43e1aa8e8324304dcedc6f3149
Kieran Chambers <kieran.chambers@icloud.com>
2024-12-04 14:48:21 +0000

    Feat: Customise FAQ form to improve UX

--- faq/forms.py
+++ faq/forms.py
@@ -6,3 +6,12 @@ class FaqForm(forms.ModelForm):
     class Meta:
         model = Faq
         fields = ('question',)
+
+    question = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'rows': 5,
+            'cols': 50,
+            'class':
+            'form-control'}),
+        help_text="Maximum length is 254 characters."
+    )
