31c6ae45f9e4171d240c4f82d99cd2f1e66fe834
Kieran Chambers <kieran.chambers@icloud.com>
2024-12-13 13:43:10 +0000

    Refactor: Remove invalid placeholder attribute from <select> tag per W3C guidelines

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,3 +1,4 @@
+
 from django import forms
 from .models import Order
 
@@ -19,16 +20,16 @@ class OrderForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
+        labels, and set autofocus on first field
         """
-        # initialising default form
+        # Initialising default form
         super().__init__(*args, **kwargs)
+
         # defining form field placeholders
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
@@ -38,15 +39,24 @@ class OrderForm(forms.ModelForm):
 
         # Sets focus to full_name field on page load
         self.fields['full_name'].widget.attrs['autofocus'] = True
+
         # iterate through fields adding asterisk to required fields
         for field in self.fields:
             if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
+                placeholder = f'{placeholders.get(field, field)} *'
             else:
-                placeholder = placeholders[field]
+                placeholder = placeholders.get(field, field)
+
             # adding placeholders to fields
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            # Do not add placeholder to 'country' field
+            if field != 'country':
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             # Adding CSS class
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             # remove default labels
             self.fields[field].label = False
+
+        # Add placeholder-like option for the country field
+        # only for select fields to prevent w3c validation error
+        if 'country' in self.fields:
+            self.fields['country'].widget.choices = [('', 'Select your country')] + list(self.fields['country'].widget.choices)[1:] # noqa
